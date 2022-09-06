4584a8bb95c5391d02a903ece3e20ae9d04659e1
dr <1561500402@qq.com>
2022-09-06 16:13:19 +0800

    feat:postcss配置完毕

---
+++ .postcssrc.js
@@ -0,0 +1,13 @@
+// 所有的postcss的插件都是一个函数
+const pxToRem = require('postcss-pxtorem')
+module.exports = {
+  //插件
+  plugins: [
+    pxToRem({
+      //设置根节点的字体大小
+      rootValue: ({ file }) => (/vant/.test(file) ? 37.5 : 75),
+      //所有属性都转成rem
+      propList: ['*']
+    })
+  ]
+}
--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,46 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+  <div>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+    <div id="box"></div>
   </div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+// git log查看commit记录
+// git status查看当前仓库的状态
+// git add.提交到暂存区
+// git commit -m
+// 1
+// -feat:新增特性
+// -
+// style:样式的修改
+// fixed:debug
+// comment:添加了注释
 
-nav {
-  padding: 30px;
+// 下载vant
+// yarn add vant@latest-v2 -S
+// 引入组件和css在main.js里面
+// 测试组件能否使用
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+// 下载rem适配插件
+// yarn add amfe-flexible
+// main.js里面引入flexible
+// 自动识别css中的px，并且转成rem  ----  post-css
+// post-css: css预处理器
+// postcss 自己能干的事情很少，需要额外下载插件
+// 下载  yarn add postcss-pxtorem
+// 新建 .postcssrc.js
+export default {}
+</script>
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+<style>
+#box {
+  width: 2rem;
+  height: 2rem;
+  background-color: aqua;
 }
 </style>
